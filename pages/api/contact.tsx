ece8d327824be1c48b763c629f7b78c9ff3b67c7
jatin_510 <jagdish@quor.com>
2021-05-04 15:45:18 +0530

    using callback function for sending the mail to improve user experience

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -5,33 +5,33 @@ dotenv.config();
 
 const { EMAIL_USERNAME, EMAIL_PASSWORD } = process.env;
 
-export default async function (req, res) {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: EMAIL_USERNAME,
-        password: EMAIL_PASSWORD,
-      },
-    });
+export default function (req, res) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: EMAIL_USERNAME,
+      password: EMAIL_PASSWORD,
+    },
+  });
 
-    const mailData = {
-      form: EMAIL_USERNAME,
-      to: EMAIL_USERNAME,
-      subject: `Message from ${req.body.name}`,
-      text: req.body.message,
-      html: <div>{req.body.message}</div>,
-    };
+  const mailData = {
+    form: EMAIL_USERNAME,
+    to: EMAIL_USERNAME,
+    subject: `Message from ${req.body.name}`,
+    text: req.body.message,
+    html: <div>{req.body.message}</div>,
+  };
 
-    const info = await transporter.sendMail(mailData);
-
-    console.log(info);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log("error in email", error);
-    res.status(500).json({ success: false });
-  }
+  transporter.sendMail(mailData, (error, data) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ success: false });
+    } else {
+      console.log(data);
+      res.status(200).json({ success: true });
+    }
+  });
 }
